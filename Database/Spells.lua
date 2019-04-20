5b235cc6940ea190c7f60b4f948434314c7163df
cloudbells <chriszmn@gmail.com>
2019-04-20 02:03:04 +0200

    Dynamically creating buttons now, added tooltip for the buttons, need to add scroll frame

---
+++ Database/Spells.lua
@@ -0,0 +1,320 @@
+-- This is a VERY temporary table - only to showcase how wide each 
+-- level of spells is and to test scroll frame functionality.
+FieldGuideMageSpells = {
+	[2] = { -- Technically level 1 but my code relies on this being 2.
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[4] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[6] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[8] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[10] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[12] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[4] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[14] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[4] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[16] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[18] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	},
+	[20] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[4] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[5] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[6] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[7] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[8] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[9] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[10] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[11] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+	},
+	[22] = {
+		[1] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[2] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[3] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[4] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		},
+		[5] = {
+			["Name"] = "Frostbolt",
+			["Rank"] = 1,
+			["Cost"] = 100,
+			["Texture"] = "Interface/ICONS/Spell_Frost_Frostbolt02",
+			["ID"] = 116
+		}
+	}
+}
--- FieldGuide.lua
+++ FieldGuide.lua
@@ -1,7 +1,32 @@
+-- TODO:
+-- 1. Scrape https://classic.wowhead.com/mage-abilities and save in file
+-- 2. Add scroll (read the book)
+-- 3. Add level fontstrings
+-- 4. Add price fontstrings (maybe fancy coin icon)
+-- 5. Add option (somewhere/somehow) to hide known spells (or maybe just hide any spells before current level)
+-- 6. Add option (somewhere/somehow) to show other classes spells
+-- 7. Add option (somewhere/somehow) to filter between certain levels?
+-- 8. Add option (somewhere/somehow) to search for a certain spell
+-- 9. Make it so that it can't be moved, and it loads in the same place as achievement tab and also make it "push out" other frames or close them, to mimic real Blizzard UI
+-- 10. At some point, make UI in XML
+-- 11. At some point in the future, port to 1.12?
+-- ?. Add to interface/addons options?
+
+-- Initializes all spell buttons.
 function initButtons()
-	
+	for level, spell in pairs(FieldGuideMageSpells) do
+		for spellIndex, spellInfo in pairs(spell) do
+			local button = CreateFrame("Button", "FieldGuideSpellButton" .. level .. spellIndex, FieldGuideFrame, "FieldGuideSpellButtonTemplate")
+			local spellTexture = _G["FieldGuideSpellButton" .. level .. spellIndex .. "IconTexture"]
+			button:SetID(spellInfo["ID"])
+			spellTexture:SetTexture(spellInfo["Texture"])
+			spellTexture:Show()
+			button:SetPoint("TOPLEFT", "FieldGuideBackgroundTexture", "TOPLEFT", (spellIndex * 45) - 10, 30 - (level * 30))
+		end
+	end
 end
 
+-- Initializes all textures.
 function initGui()
 	-- Frame --
 	FieldGuideFrame:SetWidth(768)
@@ -111,25 +136,18 @@ function initGui()
 	FieldGuideBottomLeftCornerBorderTexture:SetTexCoord(1, 0, 0, 1)
 	FieldGuideBottomLeftCornerBorderTexture:SetPoint("BOTTOMLEFT", 9, 8)
 	
-	-- Button textures --
-	-- Normal Texture
+	-- Create scroll frame for containing buttons
+	FieldGuideScrollFrame = CreateFrame("ScrollFrame", "FieldGuideScrollFrame", FieldGuideFrame)
+	-- I AM HERE -----------------------------------------------------------------------------------------------------------------------------------------------------!!!!!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	
-	
-	-- Buttons --
-	FieldGuideButtonTest = CreateFrame("CheckButton", "FieldGuideButtonTest", FieldGuideFrame)
-	FieldGuideButtonTest:SetNormalTexture("Interface/SPELLBOOK/UI-Spellbook-SpellBackground")
-	FieldGuideButtonTest:SetPushedTexture("Interface/BUTTONS/UI-Quickslot-Depress")
-	FieldGuideButtonTest:SetHighlightTexture("Interface/BUTTONS/ButtonHilight-Square")
-	FieldGuideButtonTest:SetWidth(64)
-	FieldGuideButtonTest:SetHeight(64)
-	FieldGuideButtonTest:SetPoint("CENTER")
-	FieldGuideButtonTest:SetText("Test")
-	
+	initButtons()
 	FieldGuideFrame:Show()
 end
 
+-- Sets slash commands.
 function initSlash()
 	SLASH_FIELDGUIDE1 = "/FieldGuide"
+	SLASH_FIELDGUIDE2 = "/fg"
 	SlashCmdList["FIELDGUIDE"] =
 		function()
 			if FieldGuideFrame:IsVisible() then
