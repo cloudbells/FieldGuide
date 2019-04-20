7f6b0073dd86d992f0ac1193a477dfccd1eb71d4
cloudbells <chriszmn@gmail.com>
2019-04-21 01:41:17 +0200

    Finished making everything scale dynamically - next up: turn it into a faux scroll frame

--- FieldGuide.lua
+++ FieldGuide.lua
@@ -1,4 +1,5 @@
 -- TODO:
+-- 1. PRIO NUMBER ONE: FAUXSCROLLFRAME
 -- 1. Scrape https://classic.wowhead.com/mage-abilities and save in file
 -- 2. Add class icon in top right corner
 -- 2.5. Add crenelations to scroll bar (tiny ones)
@@ -14,11 +15,15 @@
 -- 11. At some point in the future, port to 1.12?
 -- ?. Add to interface/addons options?
 
+-- BUGS:
+-- Highlighting over scroll up and down buttons is too big
+
 local Y_SPACING = 30 -- The spacing between all elements in y.
 local BUTTON_X_SPACING = 45 -- The spacing between all buttons in x.
 local BUTTON_X_START = -35 -- The x adjustment for setting start position closer to level strings.
 local LEVEL_STRING_Y_START = 40 -- The start position from top left corner for level string list in y.
 local LEVEL_STRING_X_START = 30 -- The start position from top left corner for level string list in x.
+local MAX_SCROLL = 1345
 
 local function initContent()
 	for level, spell in pairs(FieldGuideMageSpells) do -- For every level do:
@@ -36,7 +41,7 @@ local function initContent()
 			button:SetPoint("LEFT", levelString, "RIGHT", BUTTON_X_START + (level < 10 and spellIndex * BUTTON_X_SPACING + 10 or spellIndex * BUTTON_X_SPACING), 0)
 		end
 	end
-	FieldGuideFrameScrollFrameSlider:SetMinMaxValues(0, 1345)
+	FieldGuideFrameScrollFrameSlider:SetMinMaxValues(0, MAX_SCROLL)
 end
 
 -- Sets slash commands.
@@ -52,10 +57,27 @@ local function initSlash()
 	end
 end
 
+function FieldGuideScrollUp_OnClick(self, up)
+	local currentValue = FieldGuideFrameScrollFrameSlider:GetValue()
+	FieldGuideFrameScrollFrameSlider:SetValue(up and currentValue + MAX_SCROLL/15 or currentValue - MAX_SCROLL/15)
+end
+
+function FieldGuide_OnValueChanged(self)
+	local currentValue = self:GetValue()
+	self:GetParent():SetVerticalScroll(currentValue)
+	if currentValue == 0 then
+		_G[self:GetName() .. "ScrollUpButton"]:Disable()
+	elseif currentValue == MAX_SCROLL then
+		_G[self:GetName() .. "ScrollDownButton"]:Disable()
+	else
+		_G[self:GetName() .. "ScrollUpButton"]:Enable()
+		_G[self:GetName() .. "ScrollDownButton"]:Enable()
+	end
+end
+
 function FieldGuide_OnMouseWheel(self, delta)
 	local currentValue = FieldGuideFrameScrollFrameSlider:GetValue()
-	local _, maxValue = FieldGuideFrameScrollFrameSlider:GetMinMaxValues()
-	FieldGuideFrameScrollFrameSlider:SetValue(currentValue - delta * (maxValue/15))
+	FieldGuideFrameScrollFrameSlider:SetValue(currentValue - delta * (MAX_SCROLL/15))
 end
 
 function FieldGuide_OnLoad(self)
