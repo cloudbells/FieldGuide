854bf081b32efdeb6d2b33e99acdd38aa7412769
cloudbells <chriszmn@gmail.com>
2019-05-03 00:55:08 +0200

    Creates x amount of frames for reuse - need to set textures with updates

--- FieldGuide.lua
+++ FieldGuide.lua
@@ -1,6 +1,6 @@
 -- TODO:
 -- 2. Add class icon in top right corner
--- 2.75. Reuse frames/textures for when user wants to change class etc.
+-- 2.75. Reuse frames/textures for when user wants to change class etc. - make as many as the class with the most spells, then reuse these
 -- 2.9. Add right click functionality so that when user right clicks, it marks the spell for not learning/learning? Or something similar coolish - and calculate total cost of non-marked ones
 -- 4. Add price fontstrings (maybe fancy coin icon)
 -- 5. Add option (somewhere/somehow) to hide known spells (or maybe just hide any spells before current level) - IsSpellKnown(spellId, isPetSpell)
@@ -16,46 +16,55 @@
 -- add close button top right
 -- scale costs with friendly5%/honored10%/rank 3+10%? - check on pserver
 -- sort spells by cost/spec
+-- eventually, change so that we dont make 100000 fontstrings, instead make 1 and anchor it to bottom of currently hovered icon
 
 -- BUGS:
 -- Highlighting over scroll up and down buttons is too big
 
-local BUTTON_X_START = 7 -- How far to the right the buttons start.
-local BUTTON_Y_START = -55 -- How far down the buttons start.
+local BUTTON_X_START = 37 -- How far to the right the buttons start.
+local BUTTON_Y_START = -32 -- How far down the buttons start.
 local BUTTON_X_SPACING = 45 -- The spacing between all buttons in x.
 local LEVEL_STRING_X_START = 35 -- How far to the right the level strings are placed.
-local LEVEL_STRING_Y_START = -80 -- How far down the level strings are placed.
-local Y_SPACING = 45 -- The spacing between all elements in y.
-local MAX_SCROLL = 0
+local LEVEL_STRING_Y_START = -55 -- How far down the level strings are placed.
+local Y_SPACING = 0 -- The spacing between all elements in y.
 local SCROLL_AMOUNT = 0
+local NBR_OF_SPELL_ROWS = 5
 
--- Loads textures, places icons and strings in the window etc.
-local function initContent()
-	local levelCounter = 0
-	for level, spell in pairs(FieldGuideMageSpells) do -- For every level do:
-		levelCounter = levelCounter + 1
-		-- Level FontString.
-		local levelString = FieldGuideContentFrame:CreateFontString(nil, "ARTWORK", "FieldGuideLevelStringTemplate")
-		levelString:SetText(level ~= 2 and "Level " .. level or "Level 1")
-		levelString:SetPoint("TOPLEFT", LEVEL_STRING_X_START, -LEVEL_STRING_Y_START - (level * Y_SPACING))
-		for spellIndex, spellInfo in pairs(spell) do -- For every spell available at level X do:
-			-- Spell icon button.
-			local button = CreateFrame("Button", nil, FieldGuideContentFrame, "FieldGuideSpellButtonTemplate")
-			local iconTexture = button:CreateTexture(nil, "BORDER")
-			iconTexture:SetTexture(spellInfo["texture"])
-			iconTexture:SetAllPoints()
-			button:SetID(spellInfo["id"]) -- Hacky way of making tooltips work.
-			button:SetPoint("TOPLEFT", (BUTTON_X_SPACING * spellIndex) - BUTTON_X_START, -BUTTON_Y_START - (Y_SPACING * level))
-			-- Spell cost string.
-			local costString = button:CreateFontString("FieldGuideCostString" .. spellInfo["id"], "ARTWORK", "FieldGuideCostStringTemplate")
-			costString:SetText(GetCoinTextureString(spellInfo["cost"]), 12)
-			costString:SetPoint("BOTTOMLEFT", -2, -20)
-			costString:Hide()
+local levelStrings = {}
+local spellButtons = {}
+local priceString = nil
+
+local lastValue = 0
+
+local function initFrames()
+	if #spellButtons == 0 or #levelStrings == 0 then
+		local nbrOfFrames = math.floor((FieldGuideFrame:GetWidth() - (BUTTON_X_START * 2)) / BUTTON_X_SPACING) * NBR_OF_SPELL_ROWS
+		Y_SPACING = math.ceil(FieldGuideFrame:GetHeight() / NBR_OF_SPELL_ROWS) / 1.125
+		-- GC any potential old frames?
+		spellButtons = {}
+		levelStrings = {}
+		-- Create frames.
+		for i = 1, nbrOfFrames do
+			local spellButton = CreateFrame("BUTTON", nil, FieldGuideFrame, "FieldGuideSpellButtonTemplate")
+			local n = nbrOfFrames / NBR_OF_SPELL_ROWS -- The number of buttons in x.
+			local spellBtnX = BUTTON_X_START + BUTTON_X_SPACING * (i < n and i or i - math.floor(i / n) * n)
+			local spellBtnY = -Y_SPACING * math.ceil(i / n) - BUTTON_Y_START
+			spellButton:SetPoint("TOPLEFT", spellBtnX, spellBtnY)
+			--spellButton:Hide()
+			spellButtons[i] = spellButton
+			-- Create price string.
+			priceString = spellButton:CreateFontString("FieldGuidePriceString" .. i, "ARTWORK", "FieldGuideCostStringTemplate")
+			priceString:SetPoint("BOTTOMLEFT", -2, -20)
+		end
+		-- Create level strings
+		for i = 1, NBR_OF_SPELL_ROWS do
+			local levelString = FieldGuideFrame:CreateFontString(nil, "ARTWORK", "FieldGuideLevelStringTemplate")
+			levelString:SetPoint("TOPLEFT", LEVEL_STRING_X_START, -LEVEL_STRING_Y_START - (Y_SPACING * i))
+			levelString:SetText("Level 10")
+			--levelString:Hide()
+			levelStrings[i] = levelString
 		end
 	end
-	MAX_SCROLL = (levelCounter * 2 - (FieldGuideFrameScrollFrame:GetHeight() / (Y_SPACING + 0.6))) * Y_SPACING -- Why 0.6 works here is beyond me.
-	SCROLL_AMOUNT = MAX_SCROLL / (MAX_SCROLL / (Y_SPACING * 2)) -- Why does this work?
-	FieldGuideFrameScrollFrameSlider:SetMinMaxValues(0, MAX_SCROLL)
 end
 
 -- Sets slash commands.
@@ -71,9 +80,37 @@ local function initSlash()
 	end
 end
 
+-- Is called whenever user scrolls with mouse wheel or presses up/down buttons.
+function FieldGuide_UpdateButtons(self, value)
+	local currentValue = FieldGuideFrameSlider:GetValue()
+	FieldGuideFrameSlider:SetValue(currentValue - value)
+	for k, v in pairs(spellButtons) do
+		-- Update each spell button here.
+	end
+end
+
+-- Is called whenever user drags the scroll thumb manually.
+function FieldGuide_OnValueChanged(self, value)
+	value = math.floor(value)
+	if value >= lastValue + 1 then
+		lastValue = value
+	elseif value < lastValue then
+		lastValue = value
+	end
+	self:SetValue(value)
+	if value < 1 then
+		_G[self:GetName() .. "ScrollUpButton"]:Disable()
+	elseif value >= 30 then
+		_G[self:GetName() .. "ScrollDownButton"]:Disable()
+	else
+		_G[self:GetName() .. "ScrollUpButton"]:Enable()
+		_G[self:GetName() .. "ScrollDownButton"]:Enable()
+	end
+end
+
 -- Called whenever player mouses over an icon.
 function FieldGuideSpellButton_OnEnter(self)
-	_G["FieldGuideCostString" .. self:GetID()]:Show()
+	_G["FieldGuidePriceString" .. self:GetID()]:Show()
 	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
 	GameTooltip:SetHyperlink("spell:" .. self:GetID())
 end
@@ -83,37 +120,12 @@ function FieldGuideSpellButton_OnLeave(self)
 	_G["FieldGuideCostString" .. self:GetID()]:Hide()
 end
 
--- Called whenever user clicks buttons for scrolling up or down.
-function FieldGuideScroll_OnClick(self, up)
-	local currentValue = FieldGuideFrameScrollFrameSlider:GetValue()
-	FieldGuideFrameScrollFrameSlider:SetValue(up and currentValue + SCROLL_AMOUNT or currentValue - SCROLL_AMOUNT)
-end
-
--- Called whenever the value for slider has changed.
-function FieldGuide_OnValueChanged(self)
-	local currentValue = self:GetValue()
-	self:GetParent():SetVerticalScroll(currentValue)
-	if currentValue <= 0 then
-		_G[self:GetName() .. "ScrollUpButton"]:Disable()
-	elseif currentValue >= MAX_SCROLL - 1 then
-		_G[self:GetName() .. "ScrollDownButton"]:Disable()
-	else
-		_G[self:GetName() .. "ScrollUpButton"]:Enable()
-		_G[self:GetName() .. "ScrollDownButton"]:Enable()
-	end
-end
-
--- Called whenever player scrolls either direction inside the window.
-function FieldGuide_OnMouseWheel(self, delta)
-	local currentValue = FieldGuideFrameScrollFrameSlider:GetValue()
-	FieldGuideFrameScrollFrameSlider:SetValue(currentValue - delta * SCROLL_AMOUNT) -- Why does this work?
-end
-
 -- Run when addon has loaded.
 function FieldGuide_OnLoad(self)
 	self:RegisterForDrag("LeftButton")
-	initContent()
+	--initContent()
 	initSlash()
+	initFrames()
 	FieldGuideFrame:Show()
 	print("FieldGuide loaded!")
 end
