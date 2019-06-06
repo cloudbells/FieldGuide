9ee28d51d7792b56df3a8583b5d6c3a23c0c2c63
cloudbells <chriszmn@gmail.com>
2019-06-06 02:13:31 +0200

    Major refactoring, added minimap button, fixed no update on scroll bug

--- FieldGuide.lua
+++ FieldGuide.lua
@@ -1,34 +1,36 @@
--- Features:
----------------------------------------
--- 1. Add icons for tomes/quests at level 60. AQ ones, but also Mage drink quest in DM/Arcane Brilliance etc.
--- 2. (Add option (somewhere/somehow) to search for a certain spell.)
--- 3. (Add info for where to learn spells – e.g. if player is honored with only Ironforge, then tell her to go to Ironforge to train.)
--- 4. (Mark spells that the player does not wish to train and save between sessions. Perhaps make them grey?)
----------------------------------------
+--[[
+    Features:
+    ---------------------------------------
+    1. Add icons for tomes/quests at level 60. AQ ones, but also Mage drink quest in DM/Arcane Brilliance etc.
+    2. Add weapon skills.
+    3. (Add option (somewhere/somehow) to search for a certain spell.)
+    4. (Add info for where to learn spells – e.g. if player is honored with only Ironforge, then tell her to go to Ironforge to train.)
+    5. (Mark spells that the player does not wish to train and save between sessions. Perhaps make them grey?)
+    6. Fix PvP rank spell cost modification function for Classic release.
+    ---------------------------------------
 
+    Spells left to add:
+    ---------------------------------------
+    1. Priest: show all friendly faction race-specific spells, but add in tooltip which race each spell can be learned by.
+    2. Shaman: add Mail at level 40.
+    ---------------------------------------
 
--- Spells left to add:
----------------------------------------
--- Priest: show all friendly faction race-specific spells, but add in tooltip which race each spell can be learned by.
--- Shaman: add Mail at level 40.
----------------------------------------
+    Spells left to organize:
+    ---------------------------------------
+    1. Paladin: add Plate at level 40.
+    2. Hunter: add Mail at level 40.
+    3. Rogue
+    4. Druid
+    5. Warlock: add pet spells?
+    ---------------------------------------
 
-
--- Spells left to organize:
----------------------------------------
--- Paladin: add Plate at level 40.
--- Hunter: add Mail at level 40.
--- Rogue
--- Druid
--- Warlock: do I add pet spells?
----------------------------------------
-
-
--- Bugs:
----------------------------------------
--- 1. Scroll bar texture sometimes does not load.
--- 2. Highlighting over scroll up and down buttons is too big.
----------------------------------------
+    Bugs:
+    ---------------------------------------
+    1. Scroll bar texture sometimes does not load.
+    2. Highlighting over scroll up and down buttons is too big.
+    3. Ranks do not show in the tooltip (even in Classic) – add manually?
+    ---------------------------------------
+--]]
 
 local _, FieldGuide = ...
 
@@ -37,7 +39,7 @@ local selectedClass = nil -- Which class is currently selected in the dropdown. 
 local currentMinLevel = 2 -- We always start by showing level 2.
 local levelStrings = {} -- The table of FontStrings so that we can reuse these.
 local spellButtons = {} -- The table of spell buttons so that we can reuse these.
-local spellTextures = {} -- The table of spell buttons so that we can reuse these. 
+local spellTextures = {} -- The table of spell textures so that we can reuse these. 
 local classBackgrounds = { -- The name of the backgrounds in the game files.
     ["WARRIOR"] = "WarriorArms",
     ["PALADIN"] = "PaladinHoly",
@@ -62,13 +64,13 @@ local NBR_OF_SPELL_ROWS = 5 -- We want to display this many rows of spells.
 local NBR_OF_SPELL_COLUMNS = 0 -- Will be calculated later when initializing the frames.
 local NBR_OF_SPELL_BUTTONS = 0 -- Will be calculated later when initializing the frames.
 
-local function floor(value)
-    local temp 
-end
-
--- Returns true if the given class is currently selected in the dropdown list.
-local function isSelected(class)
-    return selectedClass == class
+-- Toggles the frame on and off.
+local function toggleFrame()
+    if FieldGuideFrame:IsVisible() then
+        FieldGuideFrame:Hide()
+    else
+        FieldGuideFrame:Show()
+    end
 end
 
 -- Returns true if the player is Alliance.
@@ -76,22 +78,12 @@ local function isAlliance()
     return UnitFactionGroup("player") == "Alliance"
 end
 
--- Returns the cost modifier (0.9 if player is honored or rank 3, 0.8 if both, 1 otherwise).
-local function getCostModifier()
-    local honored = false
-    -- local rankThree = UnitPVPRank("player") -- Classic exclusive code.
-    if not isAlliance() then
-        honored = select(3, GetFactionInfoByID(530)) > 5 or select(3, GetFactionInfoByID(76)) > 5 or select(3, GetFactionInfoByID(81)) > 5 or select(3, GetFactionInfoByID(68)) > 5
-    else
-        honored = select(3, GetFactionInfoByID(69)) > 5 or select(3, GetFactionInfoByID(54)) > 5 or select(3, GetFactionInfoByID(47)) > 5 or select(3, GetFactionInfoByID(72)) > 5
-    end
-    return rankThree and honored and 0.8 or honored and 0.9 or rankThree and 0.9 or 1
-end
-
--- Sets the background to the given class.
-local function setBackground(class)
-    FieldGuideFrameBackgroundTextureClass:SetTexture("Interface\\TALENTFRAME\\" .. classBackgrounds[class] .. "-TopLeft")
-    FieldGuideFrameBackgroundTextureClass:SetAlpha(0.4)
+-- Returns true if the current spell with the given spellInfo is supposed to be shown.
+local function buttonConditions(spellInfo)
+    return (FieldGuideOptions.showKnownSpells or not IsSpellKnown(spellInfo["id"])) and
+            (FieldGuideOptions.showEnemySpells or (isAlliance() and spellInfo["faction"] == 1) or
+            (not isAlliance() and spellInfo["faction"] == 2) or not spellInfo["faction"]) and
+            ((FieldGuideOptions.showTalents and spellInfo["talent"]) or not spellInfo["talent"])
 end
 
 -- Is called whenever user scrolls with mouse wheel or presses up/down buttons.
@@ -106,19 +98,15 @@ local function updateButtons(reset)
         local currentLevel = currentMinLevel + (i - 1) * 2
         levelStrings[i]:SetText(currentLevel == 2 and "Level 1" or "Level " .. currentLevel)
         for spellIndex, spellInfo in ipairs(FieldGuide[selectedClass][currentLevel]) do
-            if FieldGuideOptions.showKnownSpells or not IsSpellKnown(spellInfo["id"]) then -- Whether or not to show known spells.
-                if FieldGuideOptions.showEnemySpells or (isAlliance() and spellInfo["faction"] == 1) or -- Whether or not to show enemy faction spells.
-                        (not isAlliance() and spellInfo["faction"] == 2) or not spellInfo["faction"] then
-                    if (FieldGuideOptions.showTalents and spellInfo["talent"]) or not spellInfo["talent"] then -- Whether or not to show talents.
-                        spellTextures[counter]:SetTexture(spellInfo["texture"])
-                        spellTextures[counter]:SetAllPoints()
-                        spellButtons[counter].spellId = spellInfo["id"]
-                        spellButtons[counter].spellCost = spellInfo["cost"]
-                        spellButtons[counter]:Show()
-                        counter = counter + 1
-                        lastSpellIndex = lastSpellIndex + 1
-                    end
-                end
+            if buttonConditions(spellInfo) then
+                spellTextures[counter]:SetTexture(spellInfo["texture"])
+                spellTextures[counter]:SetAllPoints()
+                spellButtons[counter]:Hide() -- So that tooltip updates when we scroll.
+                spellButtons[counter].spellId = spellInfo["id"]
+                spellButtons[counter].spellCost = spellInfo["cost"]
+                spellButtons[counter]:Show()
+                counter = counter + 1
+                lastSpellIndex = lastSpellIndex + 1
             end
         end
         for i = counter, counter + NBR_OF_SPELL_COLUMNS - lastSpellIndex - 1 do -- Hide all unnecessary buttons.
@@ -126,6 +114,12 @@ local function updateButtons(reset)
             counter = counter + 1
         end
     end
+end
+
+-- Sets the background to the given class.
+local function setBackground(class)
+    FieldGuideFrameBackgroundTextureClass:SetTexture("Interface/TALENTFRAME/" .. classBackgrounds[class] .. "-TopLeft")
+    FieldGuideFrameBackgroundTextureClass:SetAlpha(0.4)
 end
 
 -- Changes the class to the given class.
@@ -136,17 +130,62 @@ local function setClass(dropdownButton, class)
     updateButtons(true)
 end
 
+-- Returns true if the given class is currently selected in the dropdown list.
+local function isSelected(class)
+    return selectedClass == class
+end
+
+-- Toggles the minimap button on or off.
+local function toggleMinimapButton()
+    FieldGuideOptions.minimapTable.hide = not FieldGuideOptions.minimapTable.hide
+    if FieldGuideOptions.minimapTable.hide then
+        FieldGuide.minimapIcon:Hide("FieldGuide")
+        print("Minimap button hidden. Type /fg minimap to show it again.")
+    else
+        FieldGuide.minimapIcon:Show("FieldGuide")
+    end
+end
+
 -- Sets slash commands.
 local function initSlash()
-    SLASH_FIELDGUIDE1 = "/FieldGuide"
+    SLASH_FIELDGUIDE1 = "/fieldguide"
     SLASH_FIELDGUIDE2 = "/fg"
-    SlashCmdList["FIELDGUIDE"] = function()
-        if FieldGuideFrame:IsVisible() then
-            FieldGuideFrame:Hide()
-        else
-            FieldGuideFrame:Show()
+    SlashCmdList["FIELDGUIDE"] = function(msg)
+        msg = string.lower(msg)
+        if msg == "minimap" then
+            toggleMinimapButton()
+            return
         end
+        toggleFrame()
     end
+end
+
+-- Initializes the minimap button.
+local function initMinimapButton()
+    FieldGuide.minimapIcon = LibStub("LibDBIcon-1.0")
+    local obj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("FieldGuide", {
+        type = "launcher",
+        text = "Field Guide",
+        icon = "Interface/ICONS/INV_Misc_Book_09",
+        OnClick = function(self, button)
+            if button == "LeftButton" then
+                toggleFrame()
+            elseif button == "RightButton" then
+                toggleMinimapButton()
+            end
+        end,
+        OnEnter = function(self)
+            GameTooltip:SetOwner(self, "ANCHOR_LEFT")
+            GameTooltip:AddLine("|cFFFFFFFFField Guide|r")
+            GameTooltip:AddLine("Left click to open Field Guide.")
+            GameTooltip:AddLine("Right click to hide this minimap button.")
+            GameTooltip:Show()
+        end,
+        OnLeave = function(self)
+            GameTooltip:Hide()
+        end
+    })
+    FieldGuide.minimapIcon:Register("FieldGuide", obj, FieldGuideOptions.minimapTable)
 end
 
 -- Initializes all checkboxes.
@@ -166,32 +205,15 @@ local function initCheckboxes()
     FieldGuideFrameKnownSpellsCheckBoxText:SetTextColor(1, 1, 1, 1)
     FieldGuideFrameKnownSpellsCheckBoxText:SetText("Known spells")
     FieldGuideFrameKnownSpellsCheckBox:SetPoint("RIGHT", FieldGuideFrameEnemySpellsCheckBox, "LEFT", -FieldGuideFrameKnownSpellsCheckBoxText:GetWidth(), 0)
-end
-
--- Initializes all frames, level strings, and textures for reuse.
-local function initFrames()
-    NBR_OF_SPELL_ROWS = math.floor(FieldGuideFrame:GetHeight() / 100) -- Good enough.
-    local NBR_OF_SPELL_BUTTONS = math.floor((FieldGuideFrame:GetWidth() - BUTTON_X_START * 2) / BUTTON_X_SPACING) * NBR_OF_SPELL_ROWS
-    Y_SPACING = math.ceil(FieldGuideFrame:GetHeight() / NBR_OF_SPELL_ROWS) / 1.125
-    for i = 1, NBR_OF_SPELL_BUTTONS do
-        NBR_OF_SPELL_COLUMNS = NBR_OF_SPELL_BUTTONS / NBR_OF_SPELL_ROWS -- The number of buttons in x.
-        local spellBtnX = BUTTON_X_START + BUTTON_X_SPACING * ((i - 1) % NBR_OF_SPELL_COLUMNS)
-        local spellBtnY = -Y_SPACING * math.ceil(i / NBR_OF_SPELL_COLUMNS) - BUTTON_Y_START
-        spellButtons[i] = CreateFrame("BUTTON", nil, FieldGuideFrame, "FieldGuideSpellButtonTemplate")
-        spellButtons[i]:SetPoint("TOPLEFT", spellBtnX, spellBtnY)
-        spellTextures[i] = spellButtons[i]:CreateTexture(nil, "BORDER")
-    end
-    -- Create level strings
-    for i = 1, NBR_OF_SPELL_ROWS do
-        levelStrings[i] = FieldGuideFrame:CreateFontString(nil, "ARTWORK", "FieldGuideLevelStringTemplate")
-        levelStrings[i]:SetPoint("TOPLEFT", LEVEL_STRING_X_START, -LEVEL_STRING_Y_START - Y_SPACING * i)
-    end
-    FieldGuideFrameSlider:SetMinMaxValues(0, 30 - NBR_OF_SPELL_ROWS) -- If we show 5 spell rows, the scroll max value should be 25 (it scrolls to 25th row, and shows the last 5 already).
-    tinsert(UISpecialFrames, FieldGuideFrame:GetName()) -- Allows us to close the window with escape.
+    -- Set checked or not checked.
+    FieldGuideFrameTalentsCheckBox:SetChecked(FieldGuideOptions.showTalents)
+    FieldGuideFrameEnemySpellsCheckBox:SetChecked(FieldGuideOptions.showEnemySpells)
+    FieldGuideFrameKnownSpellsCheckBox:SetChecked(FieldGuideOptions.showKnownSpells)
 end
 
 -- Initializes the dropdown menu.
 local function initDropdown()
+    selectedClass = select(2, UnitClass("player"))
     local dropdown = FieldGuideDropdownFrame
     UIDropDownMenu_Initialize(dropdown, function(self)
         local info = UIDropDownMenu_CreateInfo()
@@ -257,6 +279,52 @@ local function initDropdown()
     UIDropDownMenu_SetText(dropdown, "|c" .. RAID_CLASS_COLORS[selectedClass].colorStr .. selectedClass:sub(1, 1) .. string.lower(selectedClass:sub(2)))
 end
 
+-- Initializes all frames, level strings, and textures for reuse.
+local function initFrames()
+    NBR_OF_SPELL_ROWS = math.floor(FieldGuideFrame:GetHeight() / 100) -- Good enough.
+    Y_SPACING = math.ceil(FieldGuideFrame:GetHeight() / NBR_OF_SPELL_ROWS) / 1.125
+    FieldGuideFrameSlider:SetMinMaxValues(0, 30 - NBR_OF_SPELL_ROWS) -- If we show 5 spell rows, the scroll max value should be 25 (it scrolls to 25th row, and shows the last 5 already).
+    local NBR_OF_SPELL_BUTTONS = math.floor((FieldGuideFrame:GetWidth() - BUTTON_X_START * 2) / BUTTON_X_SPACING) * NBR_OF_SPELL_ROWS
+    -- Create spell buttons.
+    for i = 1, NBR_OF_SPELL_BUTTONS do
+        NBR_OF_SPELL_COLUMNS = NBR_OF_SPELL_BUTTONS / NBR_OF_SPELL_ROWS -- The number of buttons in x.
+        local spellBtnX = BUTTON_X_START + BUTTON_X_SPACING * ((i - 1) % NBR_OF_SPELL_COLUMNS)
+        local spellBtnY = -Y_SPACING * math.ceil(i / NBR_OF_SPELL_COLUMNS) - BUTTON_Y_START
+        spellButtons[i] = CreateFrame("BUTTON", nil, FieldGuideFrame, "FieldGuideSpellButtonTemplate")
+        spellButtons[i]:SetPoint("TOPLEFT", spellBtnX, spellBtnY)
+        spellTextures[i] = spellButtons[i]:CreateTexture(nil, "BORDER")
+    end
+    -- Create level strings.
+    for i = 1, NBR_OF_SPELL_ROWS do
+        levelStrings[i] = FieldGuideFrame:CreateFontString(nil, "ARTWORK", "FieldGuideLevelStringTemplate")
+        levelStrings[i]:SetPoint("TOPLEFT", LEVEL_STRING_X_START, -LEVEL_STRING_Y_START - Y_SPACING * i)
+    end
+end
+
+-- Initializes everything.
+local function init()
+    tinsert(UISpecialFrames, FieldGuideFrame:GetName()) -- Allows us to close the window with escape.
+    initFrames()
+    initDropdown()
+    initCheckboxes()
+    initMinimapButton()
+    initSlash()
+    setBackground(selectedClass)
+    updateButtons(true) -- Sets initial textures etc. Without this, everything is empty until we scroll.
+end
+
+-- Returns the cost modifier (0.9 if player is honored or rank 3, 0.8 if both, 1 otherwise).
+local function getCostModifier()
+    local honored = false
+    -- local rankThree = UnitPVPRank("player") -- Classic exclusive code.
+    if not isAlliance() then
+        honored = select(3, GetFactionInfoByID(530)) > 5 or select(3, GetFactionInfoByID(76)) > 5 or select(3, GetFactionInfoByID(81)) > 5 or select(3, GetFactionInfoByID(68)) > 5
+    else
+        honored = select(3, GetFactionInfoByID(69)) > 5 or select(3, GetFactionInfoByID(54)) > 5 or select(3, GetFactionInfoByID(47)) > 5 or select(3, GetFactionInfoByID(72)) > 5
+    end
+    return rankThree and honored and 0.8 or honored and 0.9 or rankThree and 0.9 or 1
+end
+
 -- Called whenever player mouses over an icon.
 function FieldGuideSpellButton_OnEnter(self)
     GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
@@ -269,11 +337,6 @@ function FieldGuideSpellButton_OnEnter(self)
         GameTooltip:AddLine("\nPrice: " .. canAfford .. priceString, nil, nil, nil, nil)
         GameTooltip:Show()
     end)
-end
-
--- Called whenever player moves mouse out of an icon.
-function FieldGuideSpellButton_OnLeave(self)
-    GameTooltip:Hide()
 end
 
 -- Is called whenever the value of the slider changes.
@@ -305,19 +368,19 @@ function FieldGuide_Scroll(delta)
     FieldGuideFrameSlider:SetValue(FieldGuideFrameSlider:GetValue() - delta)
 end
 
--- Toggles showing talents off or on. Called when the player checks/unchecks the talents checkbox.
+-- Toggles showing talents on or off Called when the player checks/unchecks the talents checkbox.
 function FieldGuide_ToggleTalents()
     FieldGuideOptions.showTalents = not FieldGuideOptions.showTalents
     updateButtons()
 end
 
--- Toggles showing enemy spells off or on. Called when the player checks/unchecks the enemy faction spells checkbox.
+-- Toggles showing enemy spells on or off. Called when the player checks/unchecks the enemy faction spells checkbox.
 function FieldGuide_ToggleEnemySpells()
     FieldGuideOptions.showEnemySpells = not FieldGuideOptions.showEnemySpells
     updateButtons()
 end
 
--- Toggles showing known spells off or on. Called when the player checks/unchecks the known spells checkbox.
+-- Toggles showing known spells on or off. Called when the player checks/unchecks the known spells checkbox.
 function FieldGuide_ToggleKnownSpells()
     FieldGuideOptions.showKnownSpells = not FieldGuideOptions.showKnownSpells
     updateButtons()
@@ -327,12 +390,6 @@ end
 function FieldGuide_OnLoad(self)
     self:RegisterForDrag("LeftButton")
     self:RegisterEvent("ADDON_LOADED")
-    initSlash()
-    initFrames()
-    initCheckboxes()
-    selectedClass = select(2, UnitClass("player"))
-    setBackground(selectedClass)
-    initDropdown()
 end
 
 -- Called on each event the frame receives.
@@ -343,14 +400,11 @@ function FieldGuide_OnEvent(self, event, ...)
                 FieldGuideOptions = {
                     showTalents = true,
                     showEnemySpells = false,
-                    showKnownSpells = true
+                    showKnownSpells = true,
+                    minimapTable = {} -- Used by LibDBIcon-1.0.
                 }
             end
-            FieldGuideFrameTalentsCheckBox:SetChecked(FieldGuideOptions.showTalents)
-            FieldGuideFrameEnemySpellsCheckBox:SetChecked(FieldGuideOptions.showEnemySpells)
-            FieldGuideFrameKnownSpellsCheckBox:SetChecked(FieldGuideOptions.showKnownSpells)
-            updateButtons(true) -- Sets initial textures etc. Without this, everything is empty until we scroll.
-            print("Field Guide loaded!")
+            init()
             self:UnregisterEvent("ADDON_LOADED")
         end
     end
